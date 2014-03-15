0ee2e44675db8dda104cd5caacc06e2f259ef833
Cameron MacFarland <distantcam@gmail.com>
2014-03-15 14:15:00 +0800

    Set up debugging of task

---
+++ src/SampleProject/SampleClass.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+
+namespace SampleProject
+{
+    class SampleClass
+    {
+    }
+}
--- src/WeavR.Tasks/Weave.cs
+++ src/WeavR.Tasks/Weave.cs
@@ -1,14 +1,42 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
+using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
 namespace WeavR.Tasks
 {
     public class Weave : Task
     {
+        [Required]
+        public ITaskItem AssemblyPath { set; get; }
+
+        [Required]
+        public string ProjectDirectory { get; set; }
+
+        [Required]
+        public string References { get; set; }
+
+        [Required]
+        public ITaskItem[] ReferenceCopyLocalPaths { get; set; }
+
+        [Required]
+        public string SolutionDir { get; set; }
+
+        public string IntermediateDir { get; set; }
+        public string KeyFilePath { get; set; }
+        public bool SignAssembly { get; set; }
+        public string DefineConstants { get; set; }
+
         public override bool Execute()
         {
-            return true;
+            if (Debugger.IsAttached)
+            {
+                Console.WriteLine("Press [ENTER] to continue.");
+                Console.ReadLine();
+            }
+
+            return false;
         }
     }
 }
