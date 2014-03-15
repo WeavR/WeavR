6a886157680426e7e62c464ee39a0765ade139c1
Cameron MacFarland <distantcam@gmail.com>
2014-03-15 12:42:45 +0800

    Created task and nuget projects

---
+++ src/WeavR.Tasks/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("WeavR.Tasks")]
+[assembly: AssemblyDescription("Build task for WeavR")]
+[assembly: AssemblyProduct("WeavR.Tasks")]
---
+++ src/WeavR.Tasks/Weave.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Build.Utilities;
+
+namespace WeavR.Tasks
+{
+    public class Weave : Task
+    {
+        public override bool Execute()
+        {
+            return true;
+        }
+    }
+}
