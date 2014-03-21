1e67c5c9a642a9dbbdba091cb6982c0a69e665fc
Cameron MacFarland <distantcam@gmail.com>
2014-03-21 10:30:57 +0800

    Merged base project with common

--- src/WeavR.Base/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("WeavR.Base")]
-[assembly: AssemblyDescription("WeavR Base classes and helpers for weavers.")]
-[assembly: AssemblyProduct("WeavR.Base")]
